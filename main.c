cf580399768a1faf9cba091d2d5b95952769a312
Brayden Edwards <edwabray@oregonstate.edu>
2024-05-21 19:12:26 -0700

    Added background process handling

--- main.c
+++ main.c
@@ -83,6 +83,49 @@ void killBackgroundProcesses() {
 } // end of "killBackgroundProcesses" function
 
 
+void checkOnBackgroundProcesses() {
+    int numOfBackgroundProcessesTerminated = 0;
+    for (int i = 0; i < numberOfBackgroundProcesses; i++) {
+        int status;
+        pid_t result = waitpid(backgroundProcesses[i], &status, WNOHANG); // getting the result of the child process
+        if (result == 0) { // the process is still running and not complete
+            continue;
+        }
+        else if (result == -1) { // there was an error.
+            perror("waitpid");
+            backgroundProcesses[i] = -1; // doing this so that it can be removed from the array
+            numOfBackgroundProcessesTerminated += 1; // if this value is greater than 0, we must reorder the array.
+        }
+        else {
+            if (WIFEXITED(status)) {
+                int exitStatus = WEXITSTATUS(status);
+                if (exitStatus == EXIT_FAILURE) {
+                    lastExitStatus = 1;
+                }
+                else {
+                    lastExitStatus = 0;
+                }
+            }
+            else {
+                lastExitStatus = 1;
+            }
+            backgroundProcesses[i] = -1; // doing this so that it can be removed from the array
+            numOfBackgroundProcessesTerminated += 1; // if this value is greater than 0, we must reorder the array.
+        }
+    } // end of for loop
+    if (numOfBackgroundProcessesTerminated > 0) { // replacing the
+        int j = 0;
+        for (int i = 0; i < numberOfBackgroundProcesses; i++) { // running a loop that reorders the array removing pid's that have been marked for termination.
+            if (backgroundProcesses[i] != -1) { // if the pid was not terminated, move it up in the array
+                backgroundProcesses[j] = backgroundProcesses[i];
+                j++;
+            }
+        } // end of for loop
+        numberOfBackgroundProcesses = j; // updating the amount of background processes that are active in the array.
+    }
+} // end of "checkOnBackgroundProcesses" function
+
+
 bool checkForComment(char firstLetterOfUserInput) {
     return firstLetterOfUserInput == '#';
 } // end of "checkForComment" function
@@ -121,6 +164,8 @@ void changeDirectory(char* path) {
         strcpy(currentWorkingDirectory, pathCopy);
     }
 } // end of "changeDirectory" function
+
+
 
 
 struct Command getUserInput() {
@@ -204,6 +249,10 @@ struct Command getUserInput() {
 
 
 void handleUserInput(struct Command cmd) {
+    if (numberOfBackgroundProcesses > 0) { // checking if there were any processes that finished since the last input.
+        checkOnBackgroundProcesses();
+    }
+
     if (cmd.isCd) {
         if (cmd.cdFilePath == NULL) { // handling the case where the user didn't pass a filepath with cd command.
             changeDirectory(home);
@@ -237,7 +286,7 @@ void handleUserInput(struct Command cmd) {
 
             if (cmd.inputFile != NULL) { // redirecting the input to a file.
                 int inputFile = open(cmd.inputFile, O_RDONLY); // opening file as read only
-                if (inputFile != -1) { // error handling for input file
+                if (inputFile == -1) { // error handling for input file
                     perror("open");
                     exit(EXIT_FAILURE);
                 }
@@ -267,6 +316,38 @@ void handleUserInput(struct Command cmd) {
                 }
             }
 
+            if (cmd.runInBackground) {
+                if (cmd.inputFile == NULL) {
+                    int devNull = open("/dev/null", O_RDONLY);
+                    if (devNull == -1) { // throwing an error if unable to open the file.
+                        perror("open");
+                        exit(EXIT_FAILURE);
+                    }
+                    if (dup2(devNull, STDIN_FILENO) == -1) { // redirecting the standard out to /dev/null and checking that there is no erro thrown
+                        perror("dup2");
+                        exit(EXIT_FAILURE);
+                    }
+                    if (close(devNull) == -1) { // closing the devNull and handling the error if there is one.
+                        perror("close");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                if (cmd.outputFile == NULL) {
+                    int devNull = open("/dev/null", O_WRONLY); // opening /dev/null in read only
+                    if (devNull == -1) { // checking that open worked properly
+                        perror("open");
+                        exit(EXIT_FAILURE);
+                    }
+                    if (dup2(devNull, STDOUT_FILENO) == -1) { // redirecting the standard out to the /dev/null file and handling the error if there is one.
+                        perror("dup2");
+                        exit(EXIT_FAILURE);
+                    }
+                    if (close(devNull) == -1) { // closing the devNull and handling the error if there is one.
+                        perror("close");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            }
             execv(cmd.commandFilePath, cmd.args);
             printf("Error in execv\n");
             perror("execv\n");
@@ -274,19 +355,25 @@ void handleUserInput(struct Command cmd) {
         }
         else { // parent process
             int status;
-            if (waitpid(pid, &status, 0) == -1) { // outputting the error of waitpid before using it
-                perror("waitpid");
-            } // wait for the child process to finish
-            if (WIFEXITED(status)) { // ensuring that the child exited normally
-                if (WEXITSTATUS(status) == EXIT_FAILURE) {
-                    lastExitStatus = 1; // setting the last exit status to the child exit status.
+            if (!cmd.runInBackground) {
+                if (waitpid(pid, &status, 0) == -1) { // outputting the error of waitpid before using it
+                    perror("waitpid");
+                } // wait for the child process to finish
+                if (WIFEXITED(status)) { // ensuring that the child exited normally
+                    if (WEXITSTATUS(status) == EXIT_FAILURE) {
+                        lastExitStatus = 1; // setting the last exit status to the child exit status.
+                    }
+                    else {
+                        lastExitStatus = 0;
+                    }
                 }
-                else {
-                    lastExitStatus = 0;
+                else { // if the child didn't exit normally, set exit status to 1
+                    lastExitStatus = 1;
                 }
             }
-            else { // if the child didn't exit normally, set exit status to 1
-                lastExitStatus = 1;
+            else {
+                backgroundProcesses[numberOfBackgroundProcesses] = pid;
+                numberOfBackgroundProcesses += 1;
             }
         }
     }
